c32a59a9158481eeb43081697c81645cd4e2e874
anil <anilmaity77@gmail.com>
2026-02-14 15:37:14 +0530

    uart code added

---
+++ SaibyaV2/saibya_v4/saibya_v4/Plug_com.ino
@@ -0,0 +1,75 @@
+
+
+void c_loop() {
+  if (Serial4.available() > 0) {
+    String command = Serial4.readStringUntil('\n'); // read one line
+    command.trim(); // remove whitespace
+
+        if (command == "PING") {
+      Serial.println("OK");
+      return;   // stop here, do not parse as motor command
+    }
+
+
+    // Check format
+    if (command.startsWith("M-")) {
+      int firstDash = command.indexOf('-');
+      int secondDash = command.indexOf('-', firstDash + 1);
+
+      if (secondDash > 0) {
+        String motorStr = command.substring(firstDash + 1, secondDash);
+        String speedStr = command.substring(secondDash + 1);
+
+        int speed = speedStr.toInt();
+
+        if (speed < -100 || speed > 100) {
+          Serial.println("Error: Speed out of range (-100 to 100)");
+          return;
+        }
+
+        if (motorStr == "A") {
+          uart_bot_speed = speed;
+                Serial.println("OK");
+
+
+        }
+        else if(motorStr == "D") {
+
+          uart_bot_direction = speed;
+      Serial.println("OK");
+
+          }
+        
+         else {
+          int motor = motorStr.toInt();
+          
+          if (motor >= 0 && motor < maxMotors) {
+
+            if(motor == 1){
+              uart_m1_speed = speed;
+            }
+            else if(motor == 2){
+                uart_m2_speed = speed;
+
+            }
+            else if(motor == 3){
+                uart_m3_speed = speed;
+
+            }
+            else if(motor == 4){
+                uart_m4_speed = speed;
+
+            }
+
+          } else {
+            Serial.println("Error: Motor number out of range");
+          }
+        }
+      } else {
+        Serial.println("Error: Invalid command format");
+      }
+    } else {
+      Serial.println("Error: Command must start with M-");
+    }
+  }
+}
